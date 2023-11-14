f34d9f04c64d2949c6205d10884a50b5619af8d7
david_calikes <david.calikes@codinstitute.net>
2023-11-14 16:16:36 +0000

    feat: Added new H1 message to press_test.html

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+/** 
+ * Listens to dom for button press and displays and hides Heading when pressed
+ * */
+
 document.addEventListener('DOMContentLoaded', function(){
     let button = document.getElementById('the-red-button');
     let heading = document.getElementById('button-message');
+    console.log("variables successfully declared")
 
     button.addEventListener('click', function() {
+        console.log("button clicked")
         heading.style.display = heading.style.display === 'none' ? 'block' : 'none';
-
+        console.log("functions executed normally")
     });
 });
-
-
-
 
 
 // function showMessage() {
